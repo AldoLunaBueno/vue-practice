0a3d12110c9e7c0cdf6d1c38dd82be64fd556bd5
AldoLunaBueno <alunab@uni.pe>
2025-03-11 23:10:22 -0500

    multiple drop zones

--- drag-n-drop-basic/src/App.vue
+++ drag-n-drop-basic/src/App.vue
@@ -7,10 +7,10 @@ export default {
       mouseYPos: 0,
       x: null,
       y: null,
-      currentZone: "zone-a", // Track the current zone
       tempFixed: false, // Temporary fixed positioning
       originaParent: null,
       canDrag: true,
+      dropZoneCount: 6,
     };
   },
   mounted() {
@@ -33,33 +33,35 @@ export default {
       this.mouseYPos = e.clientY;
     },
     handleMouseUp() {
-      if (!this.isDragging) return
-      this.isDragging = false
-      this.tempFixed = false // Restore normal positioning
+      if (!this.isDragging) return;
+      this.isDragging = false;
+      this.tempFixed = false; // Restore normal positioning
 
       // Get drop zones
-      const zoneA = this.$refs.zoneA;
-      const zoneB = this.$refs.zoneB;
       const img = this.$refs.image;
-      const zoneBRect = zoneB.getBoundingClientRect();
-
-      // Check if image is inside zoneB
-      if (
-        this.mouseXPos >= zoneBRect.left &&
-        this.mouseXPos <= zoneBRect.right &&
-        this.mouseYPos >= zoneBRect.top &&
-        this.mouseYPos <= zoneBRect.bottom
-      ) {
-        this.currentZone = "zone-b"; // Switch to zone B
-        zoneB.append(img)
-        this.x = 0; // Reset position relative to zone B
-        this.y = 0;
-        this.canDrag = false;
-      } else {
-        zoneA.append(img)
-        this.x = 0; // Reset position relative to zone A
-        this.y = 0;
-      }
+      const dropZones = this.$refs.dropZones;
+      let dropped = false;
+      dropZones.forEach((zone) => {
+        const rect = zone.getBoundingClientRect();
+        // Check if image is inside a zone
+        if (
+          this.mouseXPos >= rect.left &&
+          this.mouseXPos <= rect.right &&
+          this.mouseYPos >= rect.top &&
+          this.mouseYPos <= rect.bottom
+        ) {
+          zone.append(img);
+          this.x = 0; // Reset position relative to zone B
+          this.y = 0;
+          this.canDrag = false;
+          dropped = true
+        }        
+      });
+      if (!dropped) {
+          this.originaParent.append(img)
+          this.x = 0;
+          this.y = 0;
+        }
     },
     handleMouseDown(e) {
       if (!this.canDrag) return; // Prevent dragging if canDrag is false
@@ -68,20 +70,20 @@ export default {
       const img = this.$refs.image;
       this.originaParent = img.parentElement;
       const imgRect = img.getBoundingClientRect();
-      document.body.appendChild(img)
+      document.body.appendChild(img);
 
       // Set fixed position for smooth dragging
       img.style.position = "fixed";
       img.style.zIndex = "1000";
       this.x = imgRect.left;
-      this.y = imgRect.top;     
-    }
+      this.y = imgRect.top;
+    },
   },
 };
 </script>
 
 <template>
-  <div class="zone-a" ref="zoneA">
+  <div class="start-zone" ref="zoneA">
     <img
       alt="logo"
       src="./assets/logo.svg"
@@ -96,26 +98,12 @@ export default {
       @mousedown="handleMouseDown"
     />
   </div>
-  <div class="zone-b" ref="zoneB">
+  <div class="drop-area">
+    <div v-for="index in dropZoneCount" :key="index" class="drop-zone" ref="dropZones"></div>
   </div>
 </template>
 
 <style scoped>
-div {
-  z-index: 0;
-  position: absolute;
-  width: 100px;
-  height: 100px;
-  background-color: gray;
-  &.zone-a {
-    top: 50px;
-    left: 100px;
-  }
-  &.zone-b {
-    top: 200px;
-    left: 200px;
-  }
-}
 img {
   cursor: grab;
   position: absolute;
@@ -123,7 +111,28 @@ img {
   width: 100px;
   z-index: 1;
 }
-.fixed {
-  position: fixed;
+.start-zone {
+  width: 100px;
+  height: 100px;
+  background-color: gray;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.drop-area {
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  top: 200px;
+  left: 200px;
+  padding: 30px;
+  border: 1px solid gray;
+}
+.drop-zone {
+  width: 100px;
+  height: 100px;
+  background-color: lightblue;
+  position: relative;
 }
 </style>
